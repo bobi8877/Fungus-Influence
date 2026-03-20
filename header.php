d79eae79f93b6490813a26a524bbda0d25ac99e9
Måns <mans.jansson@hotmail.com>
2026-03-20 12:33:13 +0100

    navbar ok

---
+++ header.php
@@ -0,0 +1,16 @@
+
+    <nav>
+        <div class="navleft">
+            <a href="#">Start</a> 
+            <a href="#">About</a> 
+            <a href="#">Characters</a> 
+            <a href="#">Gallery</a> 
+        </div>
+        <div class="logodiv">
+            <img class="logo"src="fungusinfluencelogo.png">    
+        </div>
+        <div class="navright">
+            <a href="#">Contact Us</a> 
+            <a href="store.php">Store</a> 
+        </div>
+    </nav>
